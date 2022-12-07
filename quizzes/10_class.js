c78abb3b1e83f714f4421ffae372645522857e8a
kdk1116 <kevin.d.kim@intel.com>
2022-12-06 19:27:39 -0700

    class es6 exercises

--- quizzes/10_class.js
+++ quizzes/10_class.js
@@ -1,79 +1,137 @@
 import log from './pretty-log'
 
 // TODO: refactor into class syntax
-function Pixel(x = 0, y = 0) {
-  this.x = x
-  this.y = y
-  this._id = Math.random()
-}
-
-// TODO: fit into class syntax
-Pixel.prototype.distance = function(p) {
-  const {pow, sqrt} = Math
-  return sqrt(pow(this.x - p.x, 2) + pow(this.y - p.y, 2))
-}
-
-// TODO: fit into class syntax
-Pixel.prototype.midpoint = function(p) {
-  const x = (this.x + p.x) / 2.0
-  const y = (this.y + p.y) / 2.0
-  return Pixel.fromObj({x, y})
-}
-
-// TODO: fit into class syntax
-Pixel.prototype.toString = function() {
-  return `${this.id}: (x: ${this.x}, y: ${this.y})`
-}
-
-// TODO: fit into class syntax
-Pixel.fromObj = function(obj) {
-  const p = new Pixel(obj.x, obj.y)
-  return p
-}
-
-Object.defineProperty(Pixel.prototype, 'id', {
-  get() {
+class Pixel {
+  static fromObj = function(obj) {
+    const p = new Pixel(obj.x, obj.y)
+    return p
+  }
+  constructor(x=0, y=0) {
+    this.x = x
+    this.y = y
+    this._id = Math.random()
+  }
+  get id() {
     return this._id
-  },
-  set(val) {
+  }
+  set id(val) {
     this._id = val
-  },
-})
+  }
+  distance(p) {
+    const {pow ,sqrt} = Math
+    return sqrt(pow(this.x - p.x, 2) + pow(this.y - p.y, 2))
+  }
+  midpoint(p) {
+    const x = (this.x + p.x) / 2.0
+    const y = (this.y + p.y) / 2.0 
+    return Pixel.fromObj({x, y})
+  }
+  toString() {
+    return `${this.id}: (x: ${this.x}, y:${this.y})`
+  }
+}
+
+// function Pixel(x = 0, y = 0) {
+//   this.x = x
+//   this.y = y
+//   this._id = Math.random()
+// }
+
+// TODO: fit into class syntax
+// Pixel.prototype.distance = function(p) {
+//   const {pow, sqrt} = Math
+//   return sqrt(pow(this.x - p.x, 2) + pow(this.y - p.y, 2))
+// }
+
+// TODO: fit into class syntax
+// Pixel.prototype.midpoint = function(p) {
+//   const x = (this.x + p.x) / 2.0
+//   const y = (this.y + p.y) / 2.0
+//   return Pixel.fromObj({x, y})
+// }
+
+// TODO: fit into class syntax
+// Pixel.prototype.toString = function() {
+//   return `${this.id}: (x: ${this.x}, y: ${this.y})`
+// }
+
+// TODO: fit into class syntax
+// Pixel.fromObj = function(obj) {
+//   const p = new Pixel(obj.x, obj.y)
+//   return p
+// }
+
+// Object.defineProperty(Pixel.prototype, 'id', {
+//   get() {
+//     return this._id
+//   },
+//   set(val) {
+//     this._id = val
+//   },
+// })
 
 // TODO: create a Voxel class extending Pixel class
-function Voxel(x, y, z = 0) {
-  Pixel.call(this, x, y)
-  this.z = z
-}
+class Voxel extends Pixel {
+  static fromObj = function(obj) {
+    return new Voxel(obj.x, obj.y, obj.z)
+  }
+  constructor(x, y, z = 0) {
+    super(x, y)
+    this.z = z
+  }
+  distance(v) {
+    const deltaX = this.x - v.x
+    const deltaY = this.y - v.y
+    const deltaZ = this.z - v.z
 
-Voxel.prototype = Object.create(Pixel.prototype)
+    const {pow, sqrt} = Math
+    return sqrt(pow(deltaX, 2) + pow(deltaY, 2) + pow(deltaZ, 2))
+  }
+  midpoint(v) {
+    const midX = (this.x + v.x) / 2.0
+    const midY = (this.y + v.y) / 2.0
+    const midZ = (this.z + v.z) / 2.0
+    return Voxel.fromObj({x: midX, y: midY, z: midZ})
+  }
+  toString() {
+    return `${this.id}: (x: ${this.x}, y: ${this.y}, z: ${this.z})`
+  }
+
+} 
+
+// function Voxel(x, y, z = 0) {
+//   Pixel.call(this, x, y)
+//   this.z = z
+// }
+
+// Voxel.prototype = Object.create(Pixel.prototype)
 
 // TODO: fit into class syntax
-Voxel.prototype.distance = function(v) {
-  const deltaX = this.x - v.x
-  const deltaY = this.y - v.y
-  const deltaZ = this.z - v.z
+// Voxel.prototype.distance = function(v) {
+//   const deltaX = this.x - v.x
+//   const deltaY = this.y - v.y
+//   const deltaZ = this.z - v.z
 
-  const {pow, sqrt} = Math
-  return sqrt(pow(deltaX, 2) + pow(deltaY, 2) + pow(deltaZ, 2))
-}
-
-// TODO: fit into class syntax
-Voxel.prototype.midpoint = function(v) {
-  const midX = (this.x + v.x) / 2.0
-  const midY = (this.y + v.y) / 2.0
-  const midZ = (this.z + v.z) / 2.0
-  return Voxel.fromObj({x: midX, y: midY, z: midZ})
-}
-
-Voxel.prototype.toString = function() {
-  return `${this.id}: (x: ${this.x}, y: ${this.y}, z: ${this.z})`
-}
+//   const {pow, sqrt} = Math
+//   return sqrt(pow(deltaX, 2) + pow(deltaY, 2) + pow(deltaZ, 2))
+// }
 
 // TODO: fit into class syntax
-Voxel.fromObj = function(obj) {
-  return new Voxel(obj.x, obj.y, obj.z)
-}
+// Voxel.prototype.midpoint = function(v) {
+//   const midX = (this.x + v.x) / 2.0
+//   const midY = (this.y + v.y) / 2.0
+//   const midZ = (this.z + v.z) / 2.0
+//   return Voxel.fromObj({x: midX, y: midY, z: midZ})
+// }
+
+// Voxel.prototype.toString = function() {
+//   return `${this.id}: (x: ${this.x}, y: ${this.y}, z: ${this.z})`
+// }
+
+// TODO: fit into class syntax
+// Voxel.fromObj = function(obj) {
+//   return new Voxel(obj.x, obj.y, obj.z)
+// }
 
 // ----- Quiz Functions ----
 
@@ -166,13 +224,13 @@ class VoxelSOLUTION extends PixelSOLUTION {
   }
 }
 
-log()
-// testDistanceSOLUTION(),
-// testMidpointSOLUTION(),
-// testDistanceVoxelSOLUTION(),
-// testMidpointVoxelSOLUTION(),
+log(
+  testDistanceSOLUTION(),
+  testMidpointSOLUTION(),
+  testDistanceVoxelSOLUTION(),
+  testMidpointVoxelSOLUTION(),
+)
 
-// Solution Quiz functions
 
 function testDistanceSOLUTION() {
   const p = new PixelSOLUTION(5, 12)
